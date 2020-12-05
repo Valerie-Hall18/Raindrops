348a783518d09a024f1526c0282f7baa1ca2a385
Valerie Hall <valerie.hall48@k12.leanderisd.org>
2020-12-05 20:11:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+raindrops = [];
+
+function gameLoop() {
+  ctx.fillStyle = "white";
+  ctx.rect(0,0,c.width,c.height);
+  ctx.fill();
+  var raindrop = {
+    x:Math.random() * c.width,
+    y:0
+  }
+  raindrops.push(raindrop);
+  var k = raindrops.length;
+  while (k--) {
+    var drop = raindrops[k];
+    drop.y = drop.y + 3;
+    if (drop.y > c.height) {
+      raindrops.splice(k,1);
+    } else {
+      ctx.beginPath();
+      ctx.rect(drop.x,drop.y,2,4);
+      ctx.fillStyle = "purple";
+      ctx.fill();
+    }
+  }
+}
+
+setInterval(gameLoop, 50);
